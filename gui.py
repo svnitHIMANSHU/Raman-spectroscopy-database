190c7e4a38d28875f39bd06ac486138fcc332a27
Himanshu Pareek <hpareek61@gmail.com>
2017-06-12 17:25:11 +0530

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,12 +1,5 @@
 import wx
-import wx.grid
 import sqlite3
-import os
-
-def connect():
-     conn = sqlite3.connect('RAMAN.db')
-     return conn
-        
 class Example(wx.Frame):
  
     def __init__(self, parent, title):
@@ -15,39 +8,106 @@ class Example(wx.Frame):
         self.InitUI()
         self.Layout()
         self.Centre()
-        self.Show()       
+        self.Show()
  
     def InitUI(self):
-        self.grid_1 = wx.grid.Grid(self, -1, size=(11, 18))
+ 
         p = wx.Panel(self)
        
         bs = wx.BoxSizer(wx.VERTICAL)
-        self.t1 = wx.TextCtrl(p,size = (120,30),style = wx.TE_MULTILINE |wx.TE_CENTER) 
-
+        self.t1 = wx.TextCtrl(p,size = (120,30),style = wx.TE_MULTILINE |wx.TE_CENTER 
+)
         bs.Add(self.t1, 1, wx.EXPAND)
-          
-        bs.Add(grid_1, 1, wx.EXPAND)
-        
-        for e in range(0,11):
-            for f in range(0,4):
-                self.grid_1.SetCellValue(e,f,"")
-        conn = connect()
-        cursor = conn.cursor()
-        cursor.execute("SELECT SYMBOL FROM ELEMENT WHERE ROW=i AND COLUMN=j")
-        conn.commit()
-        for i in range(11):
-            for j in range(0,18):
-                cell=rows[i]
-                self.grid_1.SetCellValue(i,j,str(cell[j]))
-        btn = wx.Button(p, -1, SYMBOL, (10,20))                                   #buttons are added
-        btn.myname = SYMBOL
-        btn.Bind(wx.EVT_BUTTON, self.OnClick, btn)
-        grid_1.Add(btn, 0, wx.EXPAND)        
-        conn.close()
-        event.Skip()
+           
+        gs = wx.GridSizer(10, 13, 5, 5)
+        bs.Add(gs, 1, wx.EXPAND)
+
+conn = sqlite3.connect('RAMAN.db')
+cursor = conn.execute("SELECT SYMBOL FROM ELEMENT")
+elements = cursor.fetchall()
+   
+i = 0
+while i < 10: 
+   print elements[i]
+   i = i + 1       
+   
+i = 10
+while i <20:
+   print elements[i] 
+   i = i +1 
   
+i = 0
+while i < 10: 
+   print elements[i]
+   i = i + 1       
+   
+i = 10
+while i <20:
+   print elements[i] 
+   i = i +1 
+
+i = 20
+while i < 30: 
+   print elements[i]
+   i = i + 1       
+   
+i = 30
+while i <40:
+   print elements[i] 
+   i = i +1 
+  
+i = 40
+while i < 50: 
+   print elements[i]
+   i = i + 1       
+   
+i = 50
+while i <60:
+   print elements[i] 
+   i = i +1  
+   
+i = 60
+while i < 70: 
+   print elements[i]
+   i = i + 1       
+   
+i = 80
+while i <90:
+   print elements[i] 
+   i = i +1 
+  
+i = 90
+while i < 100: 
+   print elements[i]
+   i = i + 1       
+   
+i = 100
+while i <110:
+   print elements[i] 
+   i = i +1 
+
+i = 110
+while i < 118: 
+   print elements[i]
+   i = i + 1        
+   
+for elements in cursor:       
+    btn = wx.Button(p, -1, elements, (10,20))                              #buttons are added
+    btn.myname = elements
+    btn.Bind(wx.EVT_BUTTON, self.OnClick, btn)
+    gs.Add(btn, 0, wx.EXPAND)   
         
+    p.SetSizer(bs)
+         
+    def OnClick(self, event):                                       #When the button is clicked
+        name = event.GetEventObject().myname
+        self.t1.AppendText(name)
+        self.t1.AppendText("\n")
+   
+
+conn.close()
 app = wx.App()
 Example(None, title = 'Grid demo')
-app.MainLoop()             
+app.MainLoop()
+	            
 	
