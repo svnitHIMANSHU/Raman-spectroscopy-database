ebc53ca71f76d4e5c7840a527946e8933ee0dbc5
Himanshu Pareek <hpareek61@gmail.com>
2017-06-14 11:59:40 +0530

    Add files via upload

---
+++ LINK.py
@@ -0,0 +1,38 @@
+import sqlite3
+
+conn = sqlite3.connect('RAMAN.db')
+
+c = conn.cursor()
+
+#c.execute("CREATE table LINK(ID INT, ATOMIC NUMBER INT, MOL NUMBER INT )");
+
+#c.execute("INSERT INTO LINK VALUES (1, 1, '1')");
+
+#c.execute("INSERT INTO LINK VALUES (2, '8', '1')");
+
+#c.execute("INSERT INTO LINK VALUES (3, 6, '2')");
+
+#c.execute("INSERT INTO LINK VALUES (4, 8, '2')");
+
+#c.execute("INSERT INTO LINK VALUES (5, 11, '3')");
+
+#c.execute("INSERT INTO LINK VALUES (6, 17, '3')");
+
+#c.execute("INSERT INTO LINK VALUES (7, '5', '4')");
+
+#c.execute("INSERT INTO LINK VALUES (8, 6, '4')");
+
+#c.execute("INSERT INTO LINK VALUES (9, 13, '5')");
+
+#c.execute("INSERT INTO LINK VALUES (10, 8, '5')");
+
+c.execute("SELECT * FROM LINK")
+
+rows = c.fetchall()
+
+for row in rows:
+    print row
+
+conn.commit()
+
+conn.close()
---
+++ MOLECULE.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+conn = sqlite3.connect('RAMAN.db')
+
+c = conn.cursor()
+
+#c.execute("CREATE table MOLECULE (COMPUND NUMBER INT, COMPOUND NAME TEXT, \"COMPOUND FORMULA\" TEXT)");
+
+#c.execute("INSERT INTO MOLECULE VALUES (1, 'WATER', 'H20')");
+
+#c.execute("INSERT INTO MOLECULE VALUES (2, 'CARBON DI OXIDE', 'CO2')");
+
+#c.execute("INSERT INTO MOLECULE VALUES (3, 'SODIUM CLORIDE', 'NaCl')");
+
+#c.execute("INSERT INTO MOLECULE VALUES (4, 'BORON CARBIDE', 'BC')");
+
+#c.execute("INSERT INTO MOLECULE VALUES (5, 'ALUMINIUM OXIDE', 'AL2O3')");
+
+c.execute("SELECT * FROM MOLECULE")
+
+rows = c.fetchall()
+
+for row in rows:
+    print row
+
+conn.commit()
+
+conn.close()
