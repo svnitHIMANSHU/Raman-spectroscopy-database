0b48d2a84ea3d5c2698306aeb5d8507d79eb80bf
Himanshu Pareek <hpareek61@gmail.com>
2017-06-17 22:10:17 +0530

    Update MOLECULE.py

--- MOLECULE.py
+++ MOLECULE.py
@@ -4,7 +4,7 @@ conn = sqlite3.connect('RAMAN.db')
 
 c = conn.cursor()
 
-c.execute("CREATE table MOLECULE (MOL_NUMBER INT, MOL_NAME TEXT, \"MOL_FORMULA\" TEXT)");
+c.execute("CREATE table MOLECULE (MOL_NUMBER INT, MOL_NAME TEXT, \"MOL_FORMULA\" TEXT )");
 
 c.execute("INSERT INTO MOLECULE VALUES (1, 'WATER', 'H20')");
 
@@ -16,13 +16,15 @@ c.execute("INSERT INTO MOLECULE VALUES (4, 'BORON CARBIDE', 'BC')");
 
 c.execute("INSERT INTO MOLECULE VALUES (5, 'ALUMINIUM OXIDE', 'AL2O3')");
 
+c.execute("INSERT INTO MOLECULE VALUES (6, 'HYDROGEN PROXIDE', 'H202')");
+
 c.execute("SELECT * FROM MOLECULE")
 
-#rows = c.fetchall()
+rows = c.fetchall()
 
-#for row in rows:
-    #print row
+for row in rows:
+    print row
 
-#conn.commit()
+conn.commit()
 
-#conn.close()
+conn.close()
