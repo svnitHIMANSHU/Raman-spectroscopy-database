6ef5ec5b6b29ee3b1ec820ebd5ac4b63be880378
Himanshu Pareek <hpareek61@gmail.com>
2017-06-28 14:40:47 +0530

    Update LINK.py

--- LINK.py
+++ LINK.py
@@ -8,7 +8,7 @@ c.execute("CREATE table LINK(ID INT, ELEMENT_NUMBER INT,  MOL_NUMBER INT)");
 
 c.execute("INSERT INTO LINK VALUES (1, 1, '1')");
 
-c.execute("INSERT INTO LINK VALUES (2, '8', '1')");
+c.execute("INSERT INTO LINK VALUES (2, 8, '1')");
 
 c.execute("INSERT INTO LINK VALUES (3, 6, '2')");
 
@@ -18,7 +18,7 @@ c.execute("INSERT INTO LINK VALUES (5, 11, '3')");
 
 c.execute("INSERT INTO LINK VALUES (6, 17, '3')");
 
-c.execute("INSERT INTO LINK VALUES (7, '5', '4')");
+c.execute("INSERT INTO LINK VALUES (7, 5, '4')");
 
 c.execute("INSERT INTO LINK VALUES (8, 6, '4')");
 
@@ -28,9 +28,9 @@ c.execute("INSERT INTO LINK VALUES (10, 8, '5')");
 
 c.execute("INSERT INTO LINK VALUES (11, 1, '6')");
 
-c.execute("INSERT INTO LINK VALUES (12, '8', '6')");
+c.execute("INSERT INTO LINK VALUES (12, 8, '6')");
 
-c.execute("INSERT INTO LINK VALUES (13, '1', '7')");
+c.execute("INSERT INTO LINK VALUES (13, 1, '7')");
 
 c.execute("INSERT INTO LINK VALUES (14, 8, '7')");
 
@@ -40,7 +40,11 @@ c.execute("INSERT INTO LINK VALUES (16, 1, '8')");
 
 c.execute("INSERT INTO LINK VALUES (17, 8, '8')");
 
-c.execute("INSERT INTO LINK VALUES (18, '6', '8')");
+c.execute("INSERT INTO LINK VALUES (18, 6, '8')");
+
+c.execute("INSERT INTO LINK VALUES (19, 8, '9')");
+
+c.execute("INSERT INTO LINK VALUES (20, 6, '9')");
 
 c.execute("SELECT * FROM LINK")
 
