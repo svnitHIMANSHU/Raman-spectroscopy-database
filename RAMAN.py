1c812f02180f4805e79224bd99d2f38b41040e10
Himanshu Pareek <hpareek61@gmail.com>
2017-07-06 15:33:51 +0530

    Update RAMAN.py

--- RAMAN.py
+++ RAMAN.py
@@ -5,12 +5,31 @@ import matplotlib.pyplot as plt
 import re 
 import os 
 
+class MyFrame(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, "My Frame", size=(3000, 3000))
+        self.p = wx.Panel(self,-1)
+        wx.StaticText(self.p, -1, "What are the values of X", pos=(10, 12))
+
+        wx.CheckBox(self.p, -1, "Apples", (20,100), (160,-1))
+        wx.CheckBox(self.p, -1, "Mango", (20,150), (160,-1))
+        wx.CheckBox(self.p, -1, "Banana", (20,200), (160,-1))
+        wx.CheckBox(self.p, -1, "Orange", (20,250), (160,-1))
+        button=wx.Button(self.p,label="OK",pos=(800, 400), size = (50,50))
+        self.Bind(wx.EVT_BUTTON, self.newwindow, button)
+
+
+    def newwindow(self, event):
+        secondWindow = Example(parent=self.p)
+        secondWindow.Show()
+
 class Example(wx.Frame):
  
     def __init__(self, parent, title):
         super(Example, self).__init__(parent, title=title, size=(1000,800))
         self.inter_list = list()
-        self.plot_list = list() 
+        self.plot_list = list()
 
  
         self.InitUI()
@@ -20,16 +39,21 @@ class Example(wx.Frame):
  
     def InitUI(self):
  
-        self.p = wx.Panel(self)
-       
+        self.p = wx.Panel(self)        
         bs = wx.BoxSizer(wx.VERTICAL)
+        lbl=wx.StaticText(self.p,-1,style=wx.ALIGN_CENTER)
+        txt = "Periodic Table"
+        font = wx.Font(18, wx.ROMAN, wx.ITALIC, wx.NORMAL)
+        lbl.SetFont(font)
+	lbl.SetLabel(txt)
+	bs.Add(lbl,0,wx.ALIGN_CENTER)
         self.text = wx.TextCtrl(self.p,size = (50,30),style = wx.TE_MULTILINE |wx.TE_CENTER)
         bs.Add(self.text, 1, wx.EXPAND)
         self.list = wx.ListCtrl(self.p,size = (50,30),style = wx.LC_REPORT)
-        self.list.InsertColumn(0, 'MOLECULE NUMBER', width = 150) 
-        self.list.InsertColumn(1, 'MOLECULE NAME', wx.LIST_FORMAT_RIGHT, 200) 
-        self.list.InsertColumn(2, 'MOLECULE SYMBOL', wx.LIST_FORMAT_RIGHT, 200)
-        self.list.InsertColumn(3, 'FILE NAME', wx.LIST_FORMAT_RIGHT, 250)
+        self.list.InsertColumn(0, 'MOLECULE NUMBER',wx.LIST_FORMAT_CENTER, width = 170) 
+        self.list.InsertColumn(1, 'MOLECULE', wx.LIST_FORMAT_CENTER, 200) 
+        self.list.InsertColumn(2, 'FACILITY & REFERENCES', wx.LIST_FORMAT_CENTER, 350)
+        self.list.InsertColumn(3, 'DATA', wx.LIST_FORMAT_CENTER, 300)
         bs.Add(self.list, 1, wx.EXPAND)
         
         gs = wx.GridSizer(10, 18, 5, 5)
@@ -64,9 +88,13 @@ class Example(wx.Frame):
         self.reset_btn.Bind(wx.EVT_BUTTON, self.OnReset, self.reset_btn)
         bs.Add(self.reset_btn,0,wx.ALIGN_LEFT)
         
-        self.addnew_btn=wx.Button(self.p,-1,"Add New!")
-        self.addnew_btn.Bind(wx.EVT_BUTTON, self.OnAddNew, self.addnew_btn)
-        bs.Add(self.addnew_btn,0,wx.ALIGN_LEFT)
+        #self.addnew_btn=wx.Button(self.p,-1,"Add New!")
+        #self.addnew_btn.Bind(wx.EVT_BUTTON, self.OnAddNew, self.addnew_btn)
+        #bs.Add(self.addnew_btn,0,wx.ALIGN_LEFT)
+
+        self.total_btn=wx.Button(self.p,-1,"Total Molecule!")
+        self.total_btn.Bind(wx.EVT_BUTTON, self.OnTotal, self.total_btn)
+        bs.Add(self.total_btn,0,wx.ALIGN_RIGHT)
         
         self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.get_selected_item, self.list)
         
@@ -122,12 +150,12 @@ class Example(wx.Frame):
     def get_selected_item(self, event):
         self.plot_list = list()  
         self.plot_list.append(event.GetText())
-        print self.plot_list
+        #print self.plot_list
         
     def OnPlot(self, event):
         cursor= self.conn.execute("SELECT FILE_NAME FROM MOLECULE where MOL_NUMBER==?", (self.plot_list[0],))
         files = cursor.fetchall()
-        print files[0][0]
+        #print files[0][0]
         tf = open(files[0][0],'r+')
 	d = tf.readlines()
 	tf.seek(0)
@@ -140,65 +168,196 @@ class Example(wx.Frame):
         tf.truncate()
         tf.close()
         plt.plotfile(str(files[0][0]), delimiter=' ',comments = '#', cols=(0, 1), 
-                   names=('Raman Shift(cm-1)', 'Intensity(Arbitrary Units)'), ) 
+                   names=('Raman Shift ($\mathregular{Cm^{-1}}$)', 'Intensity (arb. units)'), ) 
         plt.show()
-        
-    def OnAddNew(self, event): 
-        dlg = GetData(parent = self)
-        dlg.ShowModal()
-        if dlg.result_name:
-            print "Atoms: "+dlg.result_atoms+"\n"
-            print "Name: "+dlg.result_name+"\n"
-            print "Formula: "+dlg.result_formula+"\n"
-            print "Fil: "+dlg.result_fil+"\n"
-        else:
-            print "No Input found\n"
-        dlg.Destroy()
 
-class GetData(wx.Dialog):
-    def __init__(self, parent):
-        bs = wx.BoxSizer(wx.VERTICAL)
-        self.parent = parent
-        wx.Dialog.__init__(self, parent, wx.ID_ANY, "New Molecule", size= (650,220))
-        self.p = wx.Panel(self,wx.ID_ANY)
-        self.lblatoms = wx.StaticText(self.p, label="Atoms", pos=(20,20))
-        self.atoms = wx.TextCtrl(self.p, value="", pos=(110,20), size=(500,-1)) 
-        bs.Add(self.atoms,0)      
-        self.lblnam = wx.StaticText(self.p, label="Name", pos=(20,60))
-        self.name = wx.TextCtrl(self.p, value="", pos=(110,60), size=(500,-1))
-        self.lblform = wx.StaticText(self.p, label="Formula", pos=(20,100))
-        self.formula = wx.TextCtrl(self.p, value="", pos=(110,100), size=(500,-1))
-        self.lblfl = wx.StaticText(self.p, label="Fil", pos=(20,140))
-        self.fil = wx.TextCtrl(self.p, value="", pos=(110,140), size=(500,-1))
-        self.saveButton =wx.Button(self.p, label="Save", pos=(110,170))
-        self.closeButton =wx.Button(self.p, label="Cancel", pos=(210,170))
-        self.saveButton.Bind(wx.EVT_BUTTON, self.SaveConnString)
-        self.closeButton.Bind(wx.EVT_BUTTON, self.OnQuit)
-        self.Bind(wx.EVT_CLOSE, self.OnQuit)
+    def OnTotal(self, event):
+        cursor= self.conn.execute("SELECT count(*) FROM MOLECULE")
+        total = cursor.fetchall()
+        print total
+
+app = wx.App(False)
+frame = MyFrame()
+frame.Show(True)
+app.MainLoop()import wx
+import sqlite3
+import itertools
+import matplotlib.pyplot as plt
+import re 
+import os 
+
+class MyFrame(wx.Frame):
+
+    def __init__(self):
+        wx.Frame.__init__(self, None, -1, "My Frame", size=(3000, 3000))
+        self.p = wx.Panel(self,-1)
+        wx.StaticText(self.p, -1, "What are the values of X", pos=(10, 12))
+
+        wx.CheckBox(self.p, -1, "Apples", (20,100), (160,-1))
+        wx.CheckBox(self.p, -1, "Mango", (20,150), (160,-1))
+        wx.CheckBox(self.p, -1, "Banana", (20,200), (160,-1))
+        wx.CheckBox(self.p, -1, "Orange", (20,250), (160,-1))
+        button=wx.Button(self.p,label="OK",pos=(800, 400), size = (50,50))
+        self.Bind(wx.EVT_BUTTON, self.newwindow, button)
+
+
+    def newwindow(self, event):
+        secondWindow = Example(parent=self.p)
+        secondWindow.Show()
+
+class Example(wx.Frame):
+ 
+    def __init__(self, parent, title):
+        super(Example, self).__init__(parent, title=title, size=(1000,800))
+        self.inter_list = list()
+        self.plot_list = list()
+
+ 
+        self.InitUI()
+        self.Layout()
+        self.Centre()
         self.Show()
+ 
+    def InitUI(self):
+ 
+        self.p = wx.Panel(self)        
+        bs = wx.BoxSizer(wx.VERTICAL)
+        lbl=wx.StaticText(self.p,-1,style=wx.ALIGN_CENTER)
+        txt = "Periodic Table"
+        font = wx.Font(18, wx.ROMAN, wx.ITALIC, wx.NORMAL)
+        lbl.SetFont(font)
+	lbl.SetLabel(txt)
+	bs.Add(lbl,0,wx.ALIGN_CENTER)
+        self.text = wx.TextCtrl(self.p,size = (50,30),style = wx.TE_MULTILINE |wx.TE_CENTER)
+        bs.Add(self.text, 1, wx.EXPAND)
+        self.list = wx.ListCtrl(self.p,size = (50,30),style = wx.LC_REPORT)
+        self.list.InsertColumn(0, 'MOLECULE NUMBER',wx.LIST_FORMAT_CENTER, width = 170) 
+        self.list.InsertColumn(1, 'MOLECULE', wx.LIST_FORMAT_CENTER, 200) 
+        self.list.InsertColumn(2, 'FACILITY & REFERENCES', wx.LIST_FORMAT_CENTER, 350)
+        self.list.InsertColumn(3, 'DATA', wx.LIST_FORMAT_CENTER, 300)
+        bs.Add(self.list, 1, wx.EXPAND)
+        
+        gs = wx.GridSizer(10, 18, 5, 5)
+        bs.Add(gs, 1, wx.EXPAND)
 
-    def OnQuit(self, event):
-        self.result_number = None
-        self.Destroy()
-
-    def SaveConnString(self, event):
-        self.result_atoms = self.atoms.GetValue()
-        self.result_name = self.name.GetValue()
-        self.result_formula = self.formula.GetValue()
-        self.result_fil = self.fil.GetValue()
         self.conn = sqlite3.connect('RAMAN.db')
-        #cursor= self.conn.execute("SELECT max(MOL_NUMBER) FROM MOLECULE")
-        #maxvalue = cursor.fetchone()[0]      
-        #cursor= self.conn.execute("INSERT INTO MOLECULE VALUES (?, ?, ?, ?);", (maxvalue + 1, "name", "formula", "fil"))
-        #cursor= self.conn.execute("SELECT max(ID) FROM LINK")
-        #supervalue = cursor.fetchone()[0]
-        # split the value of atoms
-        #cursor= self.conn.execute("INSERT INTO LINK VALUES (?,?,?);,(supervalue + 1, "result_atoms", maxvalue + 1)");
-        self.conn.commit()
-        self.Destroy()
-                
+        for row_id in range(1,11):
+           for col_id in range(1,19):
+             #print row_id,col_id
+             cursor= self.conn.execute("SELECT * FROM ELEMENT where ROW_NO==%d AND COLUMN_NO==%d"%(row_id,col_id))
+             if(cursor==None):
+                gs.Add(wx.StaticText(self.p,-1,''))
+             else:
+                elements = cursor.fetchall()
+                if(elements==None or len(elements)==0):
+                   gs.Add(wx.StaticText(self.p,-1,''))
+                else:
+                   #print elements[0]
+                   btn = wx.Button(self.p, -1,str(elements[0][1]), (10,20))                              
+                   btn.Bind(wx.EVT_BUTTON, self.OnClick, btn)
+                   gs.Add(btn, -1, wx.EXPAND)
+                   
+        self.search_btn=wx.Button(self.p,-1,"Search!")
+        self.search_btn.Bind(wx.EVT_BUTTON, self.OnSearch, self.search_btn)
+        bs.Add(self.search_btn,0,wx.ALIGN_CENTER)
+        
+        self.plot_btn=wx.Button(self.p,-1,"Plot!")
+        self.Bind(wx.EVT_BUTTON, self.OnPlot, self.plot_btn)
+        bs.Add(self.plot_btn,0,wx.ALIGN_CENTER)
+        
+        self.reset_btn=wx.Button(self.p,-1,"Reset!")
+        self.reset_btn.Bind(wx.EVT_BUTTON, self.OnReset, self.reset_btn)
+        bs.Add(self.reset_btn,0,wx.ALIGN_LEFT)
+        
+        #self.addnew_btn=wx.Button(self.p,-1,"Add New!")
+        #self.addnew_btn.Bind(wx.EVT_BUTTON, self.OnAddNew, self.addnew_btn)
+        #bs.Add(self.addnew_btn,0,wx.ALIGN_LEFT)
+
+        self.total_btn=wx.Button(self.p,-1,"Total Molecule!")
+        self.total_btn.Bind(wx.EVT_BUTTON, self.OnTotal, self.total_btn)
+        bs.Add(self.total_btn,0,wx.ALIGN_RIGHT)
+        
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.get_selected_item, self.list)
+        
+        
+        self.p.SetSizer(bs)
+        
          
-		
-app = wx.App()
-Example(None, title = 'Raman Spectroscopy Database')
-app.MainLoop()		
+    def OnClick(self, event):                                       
+        name = event.GetEventObject().GetLabelText()
+        cursor= self.conn.execute("SELECT * FROM ELEMENT where SYMBOL==?", (name,))
+        elements = cursor.fetchall()
+        #print elements
+        cursor= self.conn.execute("SELECT ATOMIC_NUMBER FROM ELEMENT where SYMBOL = ?", (name,))
+        numbers = cursor.fetchone()[0]
+        atomicnumber = numbers
+        cursor= self.conn.execute("SELECT MOL_NUMBER FROM LINK where ELEMENT_NUMBER = ?", (atomicnumber,))
+        mnumbers = cursor.fetchall()
+        #print mnumbers
+        mnum_list = []
+        for i in mnumbers:
+             mnum_list.append(i[0])
+        #print mnum_list
+        self.inter_list.append(mnum_list)
+        #print self.inter_list
+        self.molecule_list=list(set.intersection(*map(set,self.inter_list)))
+        #print self.molecule_list
+        self.text.AppendText(str(elements[0][1]))
+        self.text.AppendText("\n")
+               
+    def OnSearch(self, event):        
+        placeholder = '?'
+        placeholders = ','.join(placeholder for unused in self.molecule_list)
+        query = 'SELECT * FROM MOLECULE WHERE MOL_NUMBER IN (%s)' % placeholders
+        cursor = self.conn.execute(query, self.molecule_list)
+        final = cursor.fetchall()
+
+        no_of_elements=len(self.inter_list)
+
+        #print final         
+        for j in final:
+            mol_no=j[0]
+            cursor= self.conn.execute('SELECT * FROM LINK WHERE MOL_NUMBER = %d' % mol_no)
+            mnumbers = cursor.fetchall()
+            if(len(mnumbers)==no_of_elements):
+               self.list.Append((j[0],j[1],j[2],j[3]))                            
+
+    def OnReset(self, event):                                   
+        self.list.DeleteAllItems()
+        self.molecule_list = []
+        self.inter_list = []
+        self.text.Clear()
+        
+    def get_selected_item(self, event):
+        self.plot_list = list()  
+        self.plot_list.append(event.GetText())
+        #print self.plot_list
+        
+    def OnPlot(self, event):
+        cursor= self.conn.execute("SELECT FILE_NAME FROM MOLECULE where MOL_NUMBER==?", (self.plot_list[0],))
+        files = cursor.fetchall()
+        #print files[0][0]
+        tf = open(files[0][0],'r+')
+	d = tf.readlines()
+	tf.seek(0)
+	for line in d:
+    	     s=re.search(r'[a-zA-Z]',line)
+             if s:
+       	         tf.write('#'+line)
+    	     else:
+                 tf.write(line)
+        tf.truncate()
+        tf.close()
+        plt.plotfile(str(files[0][0]), delimiter=' ',comments = '#', cols=(0, 1), 
+                   names=('Raman Shift ($\mathregular{Cm^{-1}}$)', 'Intensity (arb. units)'), ) 
+        plt.show()
+
+    def OnTotal(self, event):
+        cursor= self.conn.execute("SELECT count(*) FROM MOLECULE")
+        total = cursor.fetchall()
+        print total
+
+app = wx.App(False)
+frame = MyFrame()
+frame.Show(True)
+app.MainLoop()
